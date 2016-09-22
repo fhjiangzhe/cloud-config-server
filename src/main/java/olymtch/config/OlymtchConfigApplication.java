8ff11f92c490713d4023c07ec44a6855e1166c7a
jiangzhe <jiangzhe@sanjiang365.com>
2016-09-22 14:25:43 +0800

    first commit

---
+++ src/main/java/olymtch/config/OlymtchConfigApplication.java
@@ -0,0 +1,12 @@
+package olymtch.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlymtchConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlymtchConfigApplication.class, args);
+	}
+}
